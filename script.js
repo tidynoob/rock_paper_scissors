2e4226dd8a487179041f5d3ef28bf02b795fcb46
tidynoob <mitchellgriffin9@gmail.com>
2022-07-30 00:40:30 -0600

    Add branch for UI

--- script.js
+++ script.js
@@ -10,90 +10,166 @@ let computerPlay = () => {
     return choices[randomIndex];
 }
 
-// test computer choice
-// let computerSelection = computerPlay();
-// console.log(computerSelection);
+let displayResults = (roundNum, result) => {
 
-// get user's choice of rock, paper, or scissors
-let getUserChoice = () => {
-    // ask the user for rock, paper, scissors
-    let userChoice = prompt("rock, paper, or scissors?", "");
-    userChoice = userChoice.toLowerCase();
-    // console.log(userChoice)
-
-    // validate the prompt selection
-    // keep asking if the answer is not valid
-    while ((!userChoice || !choices.includes(userChoice))) {
-        userChoice = prompt("please enter rock, paper, or scissors", "");
-        userChoice = userChoice.toLowerCase();
+    if (roundNumber < 2) {
+        const body = document.querySelector('body');
+        const div = document.createElement('div');
+        const results = document.createElement('p');
+        const round = document.createElement('h3');
+        results.classList.add('results');
+        round.classList.add('round');
+        // results.style.whiteSpace = 'pre-line';
+        // results.style.margin = '1rem 0rem'
+        round.textContent = roundNum;
+        results.textContent = result;
+        div.appendChild(round);
+        div.appendChild(results);
+        body.appendChild(div);
+        return
     }
-    return userChoice
+    else {
+        const results = document.querySelector('.results');
+        const round = document.querySelector('.round');
+        results.textContent = result;
+        round.textContent = roundNum;
+        return
+    }
+
 }
 
-// test user choice
-// let userSelection = getUserChoice();
-// console.log(userSelection);
 
-// compute winner of the two selections
-let playRound = (userSelection, computerSelection) => {
-    if (userSelection == computerSelection) return "It's a tie!";
-    else if (userSelection == 'rock') {
+// let displayWinner = async () => {
+//     if (userWins < 5 && computerWins < 5) return
+//     else {
+//         const div = document.querySelector('div');
+//         const winner = document.createElement('h2');
+//         if (userWins == 5) {
+//             winner.textContent = "User wins!"
+//         } else {
+//             winner.textContent = "Computer Wins!"
+//         }
+//         div.appendChild(winner);
+//     }
+// }
+
+let displayWinner = () => {
+    if (userWins < 5 && computerWins < 5) return
+    else {
+        const div = document.querySelector('div');
+        const winner = document.createElement('h2');
+        if (userWins == 5) {
+            winner.textContent = "User Wins!"
+        } else {
+            winner.textContent = "Computer Wins!"
+        }
+        div.appendChild(winner)
+    }
+}
+
+let playRound = (e) => {
+    let computerSelection = '';
+    let result = '';
+    let round = '';
+    computerSelection = computerPlay();
+    // console.log(computerSelection);
+    roundNumber++;
+    // console.log(e.target.id);
+    if (e.target.id == computerSelection) {
+        round = `Round ${roundNumber}`;
+        result = `It's a tie! \r\nUser Wins: ${userWins} \r\nComputer Wins: ${computerWins}`;
+        displayResults(round, result);
+        displayWinner();
+        playAgain();
+        return
+    }
+    else if (e.target.id == 'rock') {
         if (computerSelection == 'paper') {
-            computerWins++
-            return "Paper beats Rock. You lose!";
+            computerWins++;
+            round = `Round ${roundNumber}`;
+            result = `Paper beats Rock. You lose! \r\nUser Wins: ${userWins} \r\nComputer Wins: ${computerWins}`;
+            displayResults(round, result);
+            displayWinner();
+            setTimeout(playAgain,0);
+            return
         }
         else {
-            userWins++
-            return "Rock beats Scissors. You win!";
+            userWins++;
+            round = `Round ${roundNumber}`;
+            result = `Rock beats Scissors. You win! \r\nUser Wins: ${userWins} \r\nComputer Wins: ${computerWins}`;
+            displayResults(round, result);
+            displayWinner();
+            setTimeout(playAgain,0);
+            return
         }
     }
-    else if (userSelection == 'paper') {
+    else if (e.target.id == 'paper') {
         if (computerSelection == 'scissors') {
-            computerWins++
-            return "Scissors beats Paper. You lose!";
+            computerWins++;
+            round = `Round ${roundNumber}`;
+            result = `Scissors beats Paper. You lose! \r\nUser Wins: ${userWins} \r\nComputer Wins: ${computerWins}`;
+            displayResults(round, result);
+            displayWinner();
+            setTimeout(playAgain,0);
+            return
         }
         else {
             userWins++
-            return "Paper beats Rock. You win!";
+            round = `Round ${roundNumber}`;
+            result = `Paper beats Rock. You win! \r\nUser Wins: ${userWins} \r\nComputer Wins: ${computerWins}`;
+            displayResults(round, result);
+            displayWinner();
+            setTimeout(playAgain,0);
+            return
         }
     }
-    else if (userSelection == 'scissors') {
+    else if (e.target.id == 'scissors') {
         if (computerSelection == 'rock') {
-            computerWins++
-            return "Rock beats Scissors. You lose!";
+            computerWins++;
+            round = `Round ${roundNumber}`;
+            result = `Rock beats Scissors. You lose! \r\nUser Wins: ${userWins} \r\nComputer Wins: ${computerWins}`;
+            displayResults(round, result);
+            displayWinner();
+            setTimeout(playAgain,0);
+            return
         }
         else {
-            userWins++
-            return "Scissors beats Paper. You win!";
+            userWins++;
+            round = `Round ${roundNumber}`;
+            result = `Scissors beats Paper. You win! \r\nUser Wins: ${userWins} \r\nComputer Wins: ${computerWins}`;
+            displayResults(round, result);
+            displayWinner();
+            setTimeout(playAgain,0);
+            return
         }
+    }
+}
+
+let playAgain = () => {
+
+    if (userWins < 5 && computerWins < 5) return
+    else {
+
+        let play = confirm("Do you want to play again?");
+        if (!play) {
+            alert('Thanks for playing!');
+        } else {
+            userWins = 0;
+            computerWins = 0;
+            roundNumber = 0;
+            let div = document.querySelector('div');
+            div.remove();
+            alert('Scores have been reset!');
+        }
+
     }
 }
 
 // initialize variables for win counts
 let userWins = 0;
 let computerWins = 0;
+let roundNumber = 0;
 
-// create a game first to 3
-let playGame = () => {
+let buttons = document.querySelectorAll('button');
 
-    // set amount of wins to 0
-    userWins = 0;
-    computerWins = 0;
-
-    // keep playing rounds until User or Computer has 3 wins
-    while (userWins < 3 && computerWins < 3) {
-        alert(playRound(getUserChoice(), computerPlay()))
-    }
-
-    // say who won
-    if (userWins == 3) return "You won!"
-    else return "You lost :("
-}
-
-// Play game and keep playing game while user wants to
-let playAgain = true;
-while (playAgain) {
-    alert(playGame())
-    playAgain = confirm("Do you want to play again?")
-    if (!playAgain) alert("Thanks for playing!")
-}
+buttons.forEach(button => button.addEventListener('click', playRound));
